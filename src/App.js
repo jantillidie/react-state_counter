df42eb263be256f9f91c4095431f670d788bd9d2
Jan Maaß <git@antipost.de>
2023-11-14 16:04:43 +0100

    Add solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
+
+  function handlePlusClick() {
+    setCount(count + 1);
+  }
+
+  function handleMinusClick() {
+    setCount(count - 1);
+  }
 
   return (
     <div className="container">
@@ -10,17 +18,13 @@ export default function App() {
       <div className="button-container">
         <button
           type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
+          onClick={handleMinusClick}
         >
           -
         </button>
         <button
           type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
+          onClick={handlePlusClick}
         >
           +
         </button>
